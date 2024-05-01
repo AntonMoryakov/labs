5ca5fc3b85743e470a8cd30ef4a3553ed09cbc96
Anton <ant.v.moryakov@gmail.com>
2024-05-01 23:53:03 +0300

    Добавлены файлы из laba_1_zadanie_1

---
+++ laba_1_zadanie_1/laba_1_zadanie_1/program.cs
@@ -0,0 +1,41 @@
+using RationalNumbers;
+using System;
+
+namespace laba_1_zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Создаем несколько рациональных чисел для тестирования
+                RationalNumber a = new RationalNumber(1, 2);
+                RationalNumber b = new RationalNumber(1, 3);
+                RationalNumber c = new RationalNumber(3, 4);
+
+                // Выводим значения рациональных чисел
+                Console.WriteLine("Рациональное число a: " + a);
+                Console.WriteLine("Рациональное число b: " + b);
+                Console.WriteLine("Рациональное число c: " + c);
+
+                // Выполняем операции с рациональными числами и выводим результаты
+                Console.WriteLine("a + b = " + (a + b));
+                Console.WriteLine("a - b = " + (a - b));
+                Console.WriteLine("a * b = " + (a * b));
+                Console.WriteLine("a / b = " + (a / b));
+                Console.WriteLine("-a = " + (-a));
+                Console.WriteLine("a == b: " + (a == b));
+                Console.WriteLine("a != b: " + (a != b));
+                Console.WriteLine("a < c: " + (a < c));
+                Console.WriteLine("a > c: " + (a > c));
+                Console.WriteLine("a <= c: " + (a <= c));
+                Console.WriteLine("a >= c: " + (a >= c));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
