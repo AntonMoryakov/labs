9b75aa9f5b3c1ce4ec4b97027d65142aead4db44
Anton <ant.v.moryakov@gmail.com>
2024-05-22 22:39:28 +0300

    laba_2

---
+++ laba_2_asinc_sinc/asinc/Program.cs
@@ -0,0 +1,40 @@
+﻿// Асинхронная версия
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class async
+{
+    static async Task Main()
+    {
+        var timerForAll = new Stopwatch();
+        Console.WriteLine("     Асинхронный запрос      \n");
+        string[] sites = { "https://printotana.wordpress.com/", "https://github.com", "https://www.microsoft.com" };
+        timerForAll.Start();
+
+        var client = new HttpClient();
+        var tasks = new Task[sites.Length];
+
+        for (int i = 0; i < sites.Length; i++)
+        {
+            tasks[i] = ProcessSiteAsync(client, sites[i], i + 1);
+        }
+
+        await Task.WhenAll(tasks);
+
+        timerForAll.Stop();
+        Console.WriteLine("\n     Общее время " + timerForAll.ElapsedMilliseconds + "ms");
+    }
+
+    static async Task ProcessSiteAsync(HttpClient client, string site, int index)
+    {
+        var startTime = DateTime.Now;
+        var response = await client.GetAsync(site);
+        var content = await response.Content.ReadAsStringAsync();
+        File.WriteAllText("testAsync" + index + ".json", content);
+        var endTime = DateTime.Now;
+        Console.WriteLine("Время на запрос " + index + ": " + (endTime - startTime));
+    }
+}
---
+++ laba_2_asinc_sinc/laba_2_asinc_sinc/Program.cs
@@ -0,0 +1,31 @@
+﻿// Синхронная версия
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+
+class sync
+{
+    static void Main()
+    {
+        var timerForAll = new Stopwatch();
+        Console.WriteLine("     Синхронный запрос\n");
+        string[] sites = { "https://printotana.wordpress.com/", "https://github.com", "https://www.microsoft.com" };
+        timerForAll.Start();
+
+        var client = new HttpClient();
+
+        for (int i = 0; i < sites.Length; i++)
+        {
+            var startTime = DateTime.Now;
+            var response = client.GetAsync(sites[i]).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            File.WriteAllText("testSync" + (i + 1) + ".json", content);
+            var endTime = DateTime.Now;
+            Console.WriteLine("Время на запрос " + (i + 1) + ": " + (endTime - startTime));
+        }
+
+        timerForAll.Stop();
+        Console.WriteLine("\n     Общее время " + timerForAll.ElapsedMilliseconds + "ms");
+    }
+}
