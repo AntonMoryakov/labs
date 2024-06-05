a01f683bc984fb2f97ef261801184b9854679e59
Anton <ant.v.moryakov@gmail.com>
2024-06-05 23:25:51 +0300

    laba_3

---
+++ laba_3/Laba3/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Laba3.Models;
+
+namespace Laba3.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Employee> Employee { get; set; } = default!;
+        public DbSet<Profession> Profession { get; set; } = default!;
+        public DbSet<Emp_Prof> Emp_Prof { get; set; } = default!;
+        public DbSet<PhoneNumber> PhoneNumber { get; set; } = default!;
+
+    }
+}
